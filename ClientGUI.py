e97a5168357134e44b1bfcb6857e9380b14cff4e
Absolution <nick.redmond1987@gmail.com>
2012-04-04 17:54:52 +1000

    Update: Version 2.0 Updated socket core to asynchat & added public/private key
    ClientGUI.py/ServerGUI.py
    Removed: Asyncore as primary chat handler
    Added: Asynchat as primary chat handler
    Added: keyczar for RSA encryption/decryption of communications

--- ClientGUI.py
+++ ClientGUI.py
@@ -1,61 +1,127 @@
 # Project: Community Coding Month 2: Server/Client Chat
 # Author: Absolution
-# Version: 1.0
+# Version: 2.0
 # Client GUI
 
 from PySide.QtCore import *
 from PySide.QtGui import *
 from PySide.QtUiTools import *
+
+from keyczar.keys import RsaPrivateKey
+from keyczar.keys import RsaPublicKey
+
 import asyncore
+import asynchat
 import socket
 import threading
 import sys
 import Queue
 
 
-class ClientConnection(asyncore.dispatcher_with_send):
+class ClientConnection(asynchat.async_chat):
 
     def __init__(self, server, port, username, gui):
-        asyncore.dispatcher_with_send.__init__(self)
+        asynchat.async_chat.__init__(self)
         self.username = username
         self.create_socket(socket.AF_INET, socket.SOCK_STREAM)
         self.connect((server, port))
         self.gui = gui
+        self.data = []
+        self.set_terminator("\r\n")
         self.msgRecvd = Queue.Queue()
 
-    # On connection send the username
+        self.session_keys = None
+        self.public_key = None
+        self.server_public_key = None
+        self.create_session_keys()
+
+    # On connection
     def handle_connect(self):
-        sendUname = "uname:%s" % (self.username)
-        self.send(sendUname)
+        # Send public key to server
+        self.push(str(self.public_key) + "\r\n")
 
     # Handle closing of the socket
     def handle_close(self):
-        print "closing"
+        self.close()
+
+    def handle_excpt(self):
+        self.msgRecvd.put(self.post_Message("Server could not be contacted."))
         self.close()
 
     # Handle information coming from the socket
-    def handle_read(self):
-        data = self.recv(8192)
-        if data:
-            lines = data.split('\n')
-            for line in lines:
-                if line.startswith("ulist"):
-                    line = line.split(':')
-                    users = line[1].split(',')
-                    self.gui.qlwUsers.clear()
-                    for user in users:
-                        self.gui.qlwUsers.addItem(user.strip())
-                else:
-                    if line.strip() != '':
-                        self.msgRecvd.put(self.post_Message(line))
+    def collect_incoming_data(self, data):
+        self.data.append(data)
 
-    # Send message to the server
+    # Found self.set_terminator keyword
+    def found_terminator(self):
+        self.process_data()
+
+    # Process the data before the terminator keyword
+    def process_data(self):
+        data = "".join(self.data)
+        self.data = []
+
+        # Check if client has server public key
+        if not self.server_public_key:
+            # Check if <data> is an RSA public key
+            if data.find("publicExponent") != -1:
+                # Set server public key
+                self.set_server_public_key(data)
+                # Send client's username
+                self.send_username()
+        # Else data will be encrypted
+        else:
+            # Decrypt data with client private key
+            data = self.decrypt_data(data)
+            # Check if data is the user list
+            if data.startswith("ulist"):
+                self.update_userlist(data)
+            # Else data is chatter
+            else:
+                self.msgRecvd.put(self.post_Message(data))
+
+    def update_userlist(self, data):
+        # Strip userlist keyword from data
+        userlist = data.split(":")[1]
+        # Split users
+        userlist = userlist.split(",")
+        # Clear current GUI user list
+        self.gui.qlwUsers.clear()
+        # Add each user to GUI list
+        for user in userlist:
+            self.gui.qlwUsers.addItem(user.strip())
+
+    # Sets the server public key when received
+    def set_server_public_key(self, data):
+        self.server_public_key = RsaPublicKey.Read(data.strip())
+
+    # Send message encrypted with server public key
     def send_Message(self, message):
-        self.send(message)
+        encrypted_message = self.encrypt_data(message)
+        self.push(encrypted_message + "\r\n")
+
+    # Send username encrypted with server public key
+    def send_username(self):
+        username = "uname:%s" % (self.username)
+        encrypted_uname = self.encrypt_data(username)
+        self.push(encrypted_uname + "\r\n")
 
     # Send message to GUI thread to be placed in chat log
     def post_Message(self, message):
         return self.gui.Write_to_Log(message)
+
+    # Create private/public key pair for chat session
+    def create_session_keys(self):
+        self.session_keys = RsaPrivateKey.Generate()
+        self.public_key = self.session_keys.public_key
+
+    # Encrypt data with server public key
+    def encrypt_data(self, data):
+        return self.server_public_key.Encrypt(data.encode("UTF-8"))
+
+    # Decrypt data with client private key
+    def decrypt_data(self, data):
+        return self.session_keys.Decrypt(data)
 
 
 class ClientThread(threading.Thread):
@@ -93,24 +159,24 @@ class ChatClientGUI(object):
 
         # Associate UI Elements
             # Tab Widget
-        self.twOptions = self.widget.findChild(QTabWidget, 'twOptions')
+        self.twOptions = self.widget.findChild(QTabWidget, "twOptions")
             # Menu Items
-        self.menuConnect = self.widget.findChild(QAction, 'menuConnect')
-        self.menuDisconnect = self.widget.findChild(QAction, 'menuDisconnect')
-        self.menuExit = self.widget.findChild(QAction, 'menuQuit')
+        self.menuConnect = self.widget.findChild(QAction, "menuConnect")
+        self.menuDisconnect = self.widget.findChild(QAction, "menuDisconnect")
+        self.menuExit = self.widget.findChild(QAction, "menuQuit")
             # List Widget
-        self.qlwUsers = self.widget.findChild(QListWidget, 'qlwUsers')
+        self.qlwUsers = self.widget.findChild(QListWidget, "qlwUsers")
             # Text Boxes
-        self.teChatLog = self.widget.findChild(QTextEdit, 'teChatLog')
-        self.leUsername = self.widget.findChild(QLineEdit, 'leUsername')
-        self.leServerIP = self.widget.findChild(QLineEdit, 'leServerIP')
-        self.leServerPort = self.widget.findChild(QLineEdit, 'leServerPort')
-        self.leMessage = self.widget.findChild(QLineEdit, 'leMessage')
+        self.teChatLog = self.widget.findChild(QTextEdit, "teChatLog")
+        self.leUsername = self.widget.findChild(QLineEdit, "leUsername")
+        self.leServerIP = self.widget.findChild(QLineEdit, "leServerIP")
+        self.leServerPort = self.widget.findChild(QLineEdit, "leServerPort")
+        self.leMessage = self.widget.findChild(QLineEdit, "leMessage")
             # Push Buttons
-        self.pbSend = self.widget.findChild(QPushButton, 'pbSend')
-        self.pbConnect = self.widget.findChild(QPushButton, 'pbConnect')
-        self.pbDisconnect = self.widget.findChild(QPushButton, 'pbDisconnect')
-        self.pbExit = self.widget.findChild(QPushButton, 'pbExit')
+        self.pbSend = self.widget.findChild(QPushButton, "pbSend")
+        self.pbConnect = self.widget.findChild(QPushButton, "pbConnect")
+        self.pbDisconnect = self.widget.findChild(QPushButton, "pbDisconnect")
+        self.pbExit = self.widget.findChild(QPushButton, "pbExit")
         # Connect Objects to Functions
             # Push Buttons
         self.pbConnect.clicked.connect(self.ConnectServer)
@@ -148,7 +214,7 @@ class ChatClientGUI(object):
         self.twOptions.setCurrentIndex(0)
 
     def SendMessage(self):
-        self.startClient.client.send_Message(self.leMessage.text() + "\n")
+        self.startClient.client.send_Message(self.leMessage.text())
         self.teChatLog.append("%s: %s" % (self.username, self.leMessage.text()))
         self.leMessage.setText("")
 
--- ServerGUI.py
+++ ServerGUI.py
@@ -1,50 +1,129 @@
 # Project: Community Coding Month 2: Server/Client Chat
 # Author: Absolution
-# Version: 1.0
+# Version: 2.0
 # Server GUI
 
 from PySide.QtCore import *
 from PySide.QtGui import *
 from PySide.QtUiTools import *
+
+from keyczar.keys import RsaPrivateKey
+from keyczar.keys import RsaPublicKey
+
 import asyncore
+import asynchat
 import socket
 import threading
 import Queue
 import sys
 
 
-class Messenger(asyncore.dispatcher_with_send):
+class Messenger(asynchat.async_chat):
 
     def __init__(self, server, sock):
-        asyncore.dispatcher_with_send.__init__(self, sock)
+        asynchat.async_chat.__init__(self, sock)
         self.server = server
-        self.username = ''
+        self.set_terminator("\r\n")
 
-    def handle_read(self):
-        data = self.recv(8192)
-        if data:
-            if data.strip().startswith('uname:'):
-                username = data.strip()
-                self.username = username.split(':')[1]
-                self.server.usernames.append(self.username)
-                self.server.write_gui.put(self.server.write_to_gui_log("%s connected." % (self.username)))
-                self.new_connection()
+        self.data = []
+        self.username = ""
+        self.client_public_key = None
+        self.response = []
+
+    # Handle the information coming into the socket
+    def collect_incoming_data(self, data):
+        self.data.append(data)
+
+    # Found self.set_terminator keyword
+    def found_terminator(self):
+        print "Found terminator"
+        self.process_data()
+
+    # Process the data before the terminator keyword
+    def process_data(self):
+        data = "".join(self.data)
+        self.data = []
+
+        # Check if client public key has been received
+        if not self.client_public_key:
+            # Check if data is an RSA public key
+            if data.find("publicExponent") != -1:
+                # Set client public key
+                self.set_client_public_key(data)
+                # Send server public key
+                self.send_public_key()
+        # Else data will be encrypted
+        else:
+            # Decrypt data with server private key
+            data = self.decrypt_data(data)
+            # Check if data is a username
+            if data.startswith("uname"):
+                # Set client username
+                self.set_username(data)
+                # Send encrypted MOTD
+                self.send_MOTD()
+            # Else data is chatter
             else:
                 datatosend = "%s: %s" % (self.username, data)
                 self.server.write_gui.put(self.server.write_to_gui_log(datatosend))
                 self.server.broadcast(datatosend, self)
 
-    def handle_close(self):
-        self.server.write_gui.put(self.server.write_to_gui_log("%s left." % (self.username)))
-        self.server.broadcast("%s left the chat.\n" % (self.username), self)
-        self.server.usernames.remove(self.username)
+    def set_username(self, data):
+        # Strip whitespace from <data>
+        username = data.strip()
+        # Split keyword from data
+        self.username = username.split(":")[1]
+        # Append username to list of usernames
+        self.server.usernames.append(self.username)
+        # Write information to chat log GUI element
+        self.server.write_gui.put(self.server.write_to_gui_log("%s connected." % (self.username)))
+        # Send user list
         self.server.send_userlist()
-        self.server.client_disconnect(self)
-        asyncore.dispatcher_with_send.handle_close(self)
+        # Send user joined message
+        self.server.broadcast("%s joined the chat." % (self.username), self)
 
-    def new_connection(self):
+    # Sets the client public key when received
+    def set_client_public_key(self, data):
+        self.client_public_key = RsaPublicKey.Read(data.strip())
+
+    # Handle client leaving the chat
+    def handle_close(self):
+        # Write client that has left to chat log
+        self.server.write_gui.put(self.server.write_to_gui_log("%s left." % (self.username)))
+        # Broadcast client leaving to other clients
+        self.server.broadcast("%s left the chat.\n" % (self.username), self)
+        # Remove client username from global list
+        if self.username in self.server.usernames:
+            self.server.usernames.remove(self.username)
+        # Remove client instance from the client instance list
+        if self in self.server.clients:
+            self.server.client_disconnect(self)
+        # Send userlist to connected clients
         self.server.send_userlist()
-        self.server.message_user('Welcome to the server.\n', self)
+        # Close socket
+        self.close()
+
+    # On client connection send the user list and send MOTD
+    def send_MOTD(self):
+        MOTD = self.encrypt_data(self.server.MOTD)
+        self.push(str(MOTD) + "\r\n")
+
+    # Encrypt data using client public key and send (global)
+    def send_encrypted_data(self, data):
+        encrypt_data = self.encrypt_data(data)
+        self.push(str(encrypt_data) + "\r\n")
+
+    # Send server public key
+    def send_public_key(self):
+        self.push(str(self.server.public_key) + "\r\n")
+
+    # Encrypt data with client public key (instance)
+    def encrypt_data(self, data):
+        return self.client_public_key.Encrypt(data.encode("UTF-8"))
+
+    # Decrypt data with server private key
+    def decrypt_data(self, data):
+        return self.server.session_keys.Decrypt(data)
 
 
 class ConnectionHandler(asyncore.dispatcher):
@@ -55,41 +134,58 @@ class ConnectionHandler(asyncore.dispatcher):
         self.set_reuse_addr()
         self.bind((host, port))
         self.listen(5)
-        self.clients = []
-        self.usernames = []
         self.gui = gui
         self.write_gui = Queue.Queue()
+
         startText = "Server Started on: %s @ %s" % (host, port)
         self.write_gui.put(self.write_to_gui_log(startText))
 
+        self.MOTD = "Welcome to the server."
+        self.clients = []
+        self.usernames = []
+
+        self.session_keys = ""
+        self.public_key = ""
+        self.create_session_keys()
+
+    # Handle incoming connection from client
     def handle_accept(self):
         pair = self.accept()
         if pair is None:
             pass
         else:
             sock, addr = pair
+            # Add instance to client instance list
             self.clients.append(Messenger(self, sock))
 
+    # Generate public/private keys
+    def create_session_keys(self):
+        self.session_keys = RsaPrivateKey.Generate()
+        self.public_key = self.session_keys.public_key
+
+    # Broadcast <text> to all connected clients except <sender>
     def broadcast(self, text, sender):
         if self.usernames != None:
             for client in self.clients:
                 if client != sender:
-                    client.send(text)
+                    client.send_encrypted_data(text)
 
-    def message_user(self, text, user):
-        user.send(text)
-
+    # Send the client username list to all connected clients
     def send_userlist(self):
-        ulist = "ulist:%s\n" % (','.join(self.usernames))
+        ulist = "ulist:%s" % (",".join(self.usernames))
         for client in self.clients:
-            client.send(ulist)
+            # Encrpt using client public key and send
+            client.send_encrypted_data(ulist)
 
+    # Remove client instance on disconnection of client
     def client_disconnect(self, client):
         self.clients.remove(client)
 
+    # Write data to GUI chat log element
     def write_to_gui_log(self, data):
         return self.gui.Write_Log(data)
 
+    # Close all client sockets
     def shutdown(self):
         for client in self.clients:
             client.close()
@@ -98,6 +194,7 @@ class ConnectionHandler(asyncore.dispatcher):
         self.close()
 
 
+# Thread to allow GUI and sockets to work together
 class Server_Thread(threading.Thread):
 
     def __init__(self, host, port, gui):
@@ -111,7 +208,6 @@ class Server_Thread(threading.Thread):
             return 0
 
     def stop(self):
-        #self.server.shutdown()
         self.server.handle_close()
 
 
@@ -132,16 +228,14 @@ class ChatServerGUI(object):
 
         # Associate UI Elements
             # Menu Items
-        self.menuListen = self.widget.findChild(QAction, 'menuListen')
-        self.menuQuit = self.widget.findChild(QAction, 'menuQuit')
+        self.menuListen = self.widget.findChild(QAction, "menuListen")
+        self.menuQuit = self.widget.findChild(QAction, "menuQuit")
             # Text Edit
-        self.teActivity = self.widget.findChild(QTextEdit, 'teActivity')
+        self.teActivity = self.widget.findChild(QTextEdit, "teActivity")
             # Buttons
-        self.pbListen = self.widget.findChild(QPushButton, 'pbListen')
-        self.pbExit = self.widget.findChild(QPushButton, 'pbExit')
+        self.pbListen = self.widget.findChild(QPushButton, "pbListen")
+        self.pbExit = self.widget.findChild(QPushButton, "pbExit")
         # Connect Objects to Functions
-            # Menu Items
-        #self.menuExit.triggered.connect(self.ExitApp())
             # Buttons
         self.pbListen.clicked.connect(self.Start_Server)
         self.pbExit.clicked.connect(self.ExitApp)
@@ -152,16 +246,18 @@ class ChatServerGUI(object):
         # Launch Application
         self.app.exec_()
 
-    ## Exit App
+    # Exit App
     def ExitApp(self):
         self.server.server.close()
         self.server.join()
         sys.exit(0)
 
+    # Start the server thread
     def Start_Server(self):
         self.server = Server_Thread("localhost", 8192, self)
         self.server.start()
 
+    # Write <data> to the GUI chat log element
     def Write_Log(self, data):
         self.teActivity.append(data)
 
